a264361510db79f755cc6c75bd4e729d8cfa59cb
mnvptd <85275491+mnvptd@users.noreply.github.com>
2023-12-27 23:19:59 +0200

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,1 +1,10 @@
+function toggleTheme() {
+const body = document.body;
+const currentClass = body.className;
+if (currentClass === 'light-theme') {
 
+    body.className = 'dark-theme';
+    } else {
+        body.className = 'light-theme';
+    }
+}
